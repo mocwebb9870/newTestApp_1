ff31b29bc8b4502962a0896aba3c1af2d04107bd
mocwebb9870 <tu19ni42@yahoo.co.jp>
2021-03-21 08:12:05 +0900

    a

--- src/dao/UserDAO.java
+++ src/dao/UserDAO.java
@@ -12,56 +12,53 @@ import model.User;
 
 public class UserDAO {
 
-	private static Connection getConnection() throws URISyntaxException, SQLException{
+    private static Connection getConnection() throws URISyntaxException, SQLException {
+            // heroku configに設定されている値を取得。
+        URI dbUri = new URI(System.getenv("CLEARDB_DATABASE_URL"));
+        // :をデリミタとして必要な情報を抜き取る。
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        // JDBC用のURLを生成。
+        String dbUrl = "jdbc:mysql://" + dbUri.getHost() + dbUri.getPath();
 
-		//heroku configに設定されている値を取得
-		URI dbUri = new URI(System.getenv("CLEARDB_DATABASE_URL"));
-		//:を区切りとして必要な情報を抜き取る
-		String username = dbUri.getUserInfo().split(":")[0];
-		String password = dbUri.getUserInfo().split(":")[1];
-		//JDBC用のURLを生成
-		String dburl = "jdbc:mysql://" + dbUri.getHost() + dbUri.getPath();
-		return DriverManager.getConnection(dburl, username, password);
+        return DriverManager.getConnection(dbUrl, username, password);
+    }
 
-	}
+    public User findUser(User user) {
+        User findedUser = null;
 
-	public User findUser(User user) {
-		User findedUser = null;
-
-		try {
-			//ドライバの読み込み
-			Class.forName("com.mysql.jdbc.Driver");
-		//データベース接続のためprivateメソッドを呼び出す
+        try {
+                // ドライバの読み込み。
+            Class.forName ("com.mysql.jdbc.Driver");
+            // データベースへ接続のためprivateメソッドを呼び出す。
             try(Connection conn = getConnection()) {
-				//SELECT文を準備
-				String sql = "SELECT * FROM account WHERE nickName = ? AND password = ?";
-				PreparedStatement pStmt = conn.prepareStatement(sql);
-				pStmt.setString(1, user.getNickName());
-				pStmt.setString(2, user.getPassword());
-
-				//SELECTを実行し、結果表を取得
-				ResultSet rs = pStmt.executeQuery();
-
-				//一致したユーザーが存在した場合、
-				//そのユーザーを表すfindedUserインスタンスを生成
-				if(rs.next()) {
-					//結果表からデータを取得
-					String nickName = rs.getString("nickName");
-					String password = rs.getString("password");
-					findedUser = new User(nickName, password);
-				}
-			}catch (URISyntaxException e) {
-				e.printStackTrace();
-				return null;
-			}catch (SQLException e) {
-				e.printStackTrace();
-				return null;
-			}
-		}catch (ClassNotFoundException e1) {
-			e1.printStackTrace();
-			return null;
-		}
-		return findedUser;
-	}
+                // SELECT文を準備。
+                String sql = "SELECT * FROM account WHERE nickname = ? AND password = ?";
+                PreparedStatement pStmt = conn.prepareStatement(sql);
+                pStmt.setString(1, user.getNickName());
+                pStmt.setString(2, user.getPassword());
+                // SELECTを実行し、結果表を取得。
+                ResultSet rs = pStmt.executeQuery();
+                // 一致したユーザーが存在した場合、
+                // そのユーザーを表すfindedUserインスタンスを生成。
+                if (rs.next()) {
+                // 結果表からデータを取得
+                    String nickName = rs.getString("nickname");
+                    String password = rs.getString("password");
+                    findedUser = new User(nickName, password);
+                }
+            } catch (URISyntaxException e) {
+                e.printStackTrace();
+                return null;
+            } catch (SQLException e) {
+                e.printStackTrace();
+                return null;
+            }
+        } catch (ClassNotFoundException e1) {
+            e1.printStackTrace();
+            return null;
+        }
+        return findedUser;
+    }
 
 }
